3aeceb43789662e0ed61e901a59355be62c0367f
Shohei-Ikeda <s.ikeda@wardish.com>
2019-12-18 17:18:13 +0900

    first commit

---
+++ codeigniter/application/controllers/News.php
@@ -0,0 +1,53 @@
+<?php
+class News extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        // データベース接続モデルをロード
+        $this->load->model('news_model');
+    }
+    public function index() {
+        // 登録されているデータを全件取得
+        $data['news'] = $this->news_model->get_news();
+        
+        $data['title'] = 'News archive';
+        
+        // 各種viewを呼び出す
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/index', $data);
+        $this->load->view('templates/footer');
+        
+    }
+    public function view($slug = NULL) {
+        // 指定された記事を呼び出す
+        $data['news_item'] = $this->news_model->get_news($slug);
+        
+        // 記事が見つからない場合は404エラー
+        if (empty($data['news_item'])) {
+                show_404();
+        }
+        $data['title'] = $data['news_item']['title'];
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/view', $data);
+        $this->load->view('templates/footer');
+    }
+    
+    // ここまで、前回までに作っていた箇所
+    
+    // ここから、記事の作成部分
+    public function create() {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $data['title'] = 'Create a news item';
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'text', 'required');
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('news/create');
+            $this->load->view('templates/footer');
+        } else {
+            $this->news_model->set_news();
+            $this->load->view('news/success');
+        }
+    }
+    
+}
